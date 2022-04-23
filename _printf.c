f55cd270df2a6f8321b7070c4b166ff26adb401e
Celine Amarachi <94835448+CelineAma@users.noreply.github.com>
2022-04-23 01:10:03 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -16,7 +16,6 @@ int _printf(const char *format, ...)
 	va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);
 	if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
 		return (-1);
-
 	if (!format[i])
 		return (0);
 	for (i = 0; format && format[i]; i++)
